1a3361869323d26ff4f59d7d40579ffc4541d806
Julian K. <julikows+gh@gmail.com>
2025-01-19 07:44:12 +0100

    yes

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,51 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
 import { init, read } from '../wasm/pkg'
-import { onMounted, ref, useTemplateRef } from 'vue';
+import { onMounted, Ref, ref, useTemplateRef } from 'vue';
 import SWReload from './components/SWReload.vue';
+import Droptarget from './components/Droptarget.vue';
+
 onMounted(() => {
 	init()
 })
-function fileadd() {
-	if (!inputelem.value || !inputelem.value.files || !inputelem.value.files[0]) return
-	let url = URL.createObjectURL(inputelem.value.files[0])
-	console.log(url)
-	videourl.value = url
 
-}
+// refs
+let videoelem = useTemplateRef("videoelem")
 
-let videourl = ref("");
+
+
+//options
 let threshold = ref(40);
 let frameinterval = ref(10);
-let videoelem = useTemplateRef("videoelem")
-let inputelem = useTemplateRef("inputelem")
-let imgelem = useTemplateRef("targetimg")
-let counter = 0; // no ref
+let stage: Ref<"file" | "options" | "read" | "generate" | "result"> = ref("file");
+let resultblob = ref("");
+
+let file = ref<File | null>(null);
+
+// let videoelem = document.createElement("video")
+
+function filechange() {
+	console.log(file.value)
+	if(file.value) {
+		let blob = URL.createObjectURL(file.value);
+		if (!videoelem.value) return;
+		videoelem.value.src = blob;
+		stage.value = "options"
+	} else {
+		stage.value = "file"
+	}
+}
+
+
+
 let canvas = document.createElement("canvas")
 let twod = canvas.getContext("2d", { alpha: false, willReadFrequently: true })
+
+let counter = 0; // no ref
+
 let dimensions = { width: 0, height: 0 }
+
 async function canplay() {
 	if (!videoelem.value) return
-	videoelem.value.style.display = ""
 	let height = videoelem.value?.videoHeight ? videoelem.value?.videoHeight : 0;
 	let width = videoelem.value?.videoWidth ? videoelem.value?.videoWidth : 0;
 	canvas.height = height;
@@ -38,11 +57,13 @@ async function canplay() {
 	await videoelem.value.play()
 	videoelem.value.pause()
 }
+
 async function start() {
+	stage.value = "read"
 	if (!videoelem.value) return
 	images = []
 	let vid = videoelem.value
-	if (imgelem.value) imgelem.value.src = ""
+	resultblob.value = ""
 	vid.currentTime = 0
 	let frametime = 1 / frameinterval.value
 	while (vid.currentTime + frametime + 0.05 < vid.duration) {
@@ -52,11 +73,12 @@ async function start() {
 		vid.pause()
 		await delay((frametime * 1000))
 		let img = await capture()
-		if(img) images.push(img)
+		if (img) images.push(img)
 		await delay(5)
 	}
 	videoend()
 }
+
 let rtiv = 0;
 let rtcurrent = new Uint8Array();
 async function startRt() {
@@ -64,15 +86,16 @@ async function startRt() {
 	let cam = await navigator.mediaDevices.getUserMedia({ video: true })
 	videoelem.value.srcObject = cam
 	let first = await capture();
-	if(!first) return;
+	if (!first) return;
 	rtcurrent = first;
 	rtiv = setInterval(async () => {
 		let img = await capture()
 		if (!img) return;
-		
+
 
 	}, (1 / frameinterval.value) * 1000)
 }
+console.log({ rtiv, rtcurrent })
 //function stopRt() {
 //	clearInterval(rtiv);
 //}
@@ -81,12 +104,14 @@ async function startRt() {
 function delay(time: number) {
 	return new Promise((resolve) => setTimeout(resolve, time))
 }
+
 let images: Uint8Array[] = [];
+
 async function capture() {
 	if (!videoelem.value || !twod) return;
 	if (counter % frameinterval.value != 0) return;
 	let { width, height } = dimensions
-	twod.clearRect(0, 0, width, height)
+	//twod.clearRect(0, 0, width, height)
 	twod.drawImage(videoelem.value, 0, 0, width, height)
 	let idata = twod.getImageData(0, 0, width, height);
 	let img = new Uint8Array(idata.data.buffer);
@@ -97,38 +122,90 @@ async function capture() {
 }
 function videoend() {
 	console.log("END")
-	if (videoelem.value) videoelem.value.style.display = "none"
+	stage.value = "generate"
 	let result = read(images, dimensions.width, dimensions.height, true, threshold.value)
+	stage.value = "result"
 	let url = URL.createObjectURL(new Blob([result], { type: "image/png" }))
-	if (imgelem.value && videoelem.value) {
-		//videoelem.value.style.display = "none"
-		//videoelem.value.src = ""
-		imgelem.value.src = url
+	resultblob.value = url
+}
+function download() {
+	let link = document.createElement("a");
+	link.href = resultblob.value
+	let name = "";
+	if(file.value && file.value.name) {
+		let split = file.value.name.split('.')
+		split.pop()
+		split.push("png")
+		name = split.join(".")
+	} else {
+		name = "Stroboskopbild.png"
 	}
+	link.download = name
+	link.click()
+}
+function restart() {
+	file.value = null
+	stage.value = 'file'
 }
 
 </script>
 
 <template>
 	<SWReload />
-	<span>Threshold {{ threshold }}: </span>
-	<br>
-	<input type="number" name="" id="" min="0" max="255" :value="threshold"
-		@input="e => threshold = parseInt((e.target as HTMLInputElement).value)">
-	<br>
-	<span>%Frame {{ frameinterval }}</span>
-	<br>
-	<input type="number" name="" id="" min="0" max="60" :value="frameinterval"
-		@input="e => frameinterval = parseInt((e.target as HTMLInputElement).value)">
-	<br>
-	<input type="file" name="" id="" @change="fileadd" ref="inputelem">
-	<br>
-	<button @click="start">Start</button>
-	<button @click="startRt">RT</button>
-	<br>
-	<video controls :src="videourl" ref="videoelem" @canplaythrough="canplay"></video>
-	<img src="" alt="" ref="targetimg">
-	<HelloWorld msg="Vite + Vue" />
+	<div class="spacer"></div>
+	<Droptarget @changed="filechange" v-show="stage == 'file' || stage == 'options'" v-model="file"/>
+	<div class="spacer center">
+		<button @click="startRt" v-if="stage == 'file'" class="center">🎥 Webcam verwenden</button>
+	</div>
+	<div class="options" v-show="stage == 'options'">
+		<span>Schwellwert</span>
+		<br>
+		<input type="number" name="" id="" min="0" max="255" :value="threshold"
+			@input="e => threshold = parseInt((e.target as HTMLInputElement).value)">
+		<br>
+		<span>Bildwiderholrate</span>
+		<br>
+		<input type="number" name="" id="" min="0" max="60" :value="frameinterval"
+			@input="e => frameinterval = parseInt((e.target as HTMLInputElement).value)">
+		<div class="spacer"></div>
+		<button @click="start">Start</button>
+		<br>
+	</div>
+	<video src="" ref="videoelem" @canplaythrough="canplay" v-show="stage == 'read'"></video>
+	<span class="center" v-if="stage == 'generate'">Stroboskopbild wird erstellet...</span>
+	<img :src="resultblob" alt="" v-if="stage == 'result'" class="result">
+	<button class="center" @click="download" v-if="stage == 'result'">💾 Download</button>
+	<div class="spacer"></div>
+	<button class="center" @click="restart" v-if="stage == 'result'">Weiters Bild erstellen</button>
 </template>
 
-<style scoped></style>
+<style scoped>
+.options {
+	margin: auto;
+	width: fit-content;
+	margin-top: 1rem;
+	text-align: center;
+}
+
+.spacer {
+	margin-top: .5rem;
+}
+
+.center {
+	text-align: center;
+	display: block;
+	width: fit-content;
+	margin-left: auto;
+	margin-right: auto;
+}
+
+video {
+	margin: 0 .5rem;
+	max-width: calc(100vw - 1rem);
+}
+
+.result {
+	margin: 0 .5rem;
+	max-width: calc(100vw - 1rem);
+}
+</style>
---
+++ src/components/Droptarget.vue
@@ -0,0 +1,126 @@
+<script lang="ts" setup>
+import { ref, useModel, useTemplateRef, watch } from 'vue';
+
+const dragged = ref(false);
+const fileElem = useTemplateRef("file")
+
+const model = defineModel<File | null>()
+
+const haveFile = ref(false);
+
+const errorText = ref("");
+
+const emit = defineEmits({ changed() { return true; } })
+
+function addFileInput() {
+	console.log(fileElem.value)
+	if (!fileElem.value || !fileElem.value.files || !fileElem.value.files[0]) return
+	let files = fileElem.value.files;
+	if (files.length != 1) {
+		error("Nur eine Datei gleichzeitig möglich");
+		return;
+	}
+
+	addFile(files[0])
+}
+function addFile(file: File) {
+	console.log(file)
+	if (!file.type.startsWith("video")) {
+		error("keine Videodatei")
+		return;
+	}
+	haveFile.value = true;
+	model.value = file;
+	emit('changed')
+}
+
+function clear() {
+	haveFile.value = false;
+	model.value = null;
+	emit("changed")
+}
+
+watch(model, (newV, _) => {
+	haveFile.value = !!newV;
+})
+
+function dropped(ev: DragEvent) {
+	ev.preventDefault()
+	dragged.value = false;
+	if (!ev.dataTransfer) return;
+	let files = ev.dataTransfer.items ? [...ev.dataTransfer.items].filter(x => x.kind == "file").map(x => x.getAsFile()) : [...ev.dataTransfer.files].map(x => x);
+	if (files.length != 1) {
+		error("Nur eine Datei gleichzeitig möglich");
+		return;
+	}
+	if (files[0]) addFile(files[0])
+}
+
+function error(msg: string) {
+	errorText.value = msg
+	setTimeout(() => {
+		errorText.value = ""
+		dragged.value = false;
+	}, 5000)
+}
+
+</script>
+<template>
+	<div class="target" @dragenter="dragged = true" @dragleave="dragged = false" @dragover.stop.prevent
+		@drop.stop.prevent="dropped" :class="{ dragged, haveFile }">
+		<input id="" ref="file" type="file" name="" style="display: none" @change="addFileInput" accept="video/*">
+		<div class="center" v-if="!haveFile" @dragenter="dragged = true" @dragleave="dragged = false"
+			@drop.prevent="dropped">
+			<span>Datei hierher ziehen</span>
+			<span>oder</span>
+			<button class="select" @click="fileElem?.click">📼 Video Auswählen</button>
+		</div>
+		<div class="center" v-if="haveFile">
+			<button @click="clear">Datei Entfernen</button>
+		</div>
+		<div class="center" v-if="errorText">
+			<span class="error">{{ errorText }}</span>
+		</div>
+	</div>
+</template>
+
+<style scoped>
+.target {
+	width: 50vw;
+	min-width: 5rem;
+	margin: auto;
+
+	display: block;
+}
+
+.target:not(.haveFile) {
+	height: 10rem;
+	max-height: 50vh;
+	border: 1px dashed #777;
+	border-radius: 5px;
+}
+
+
+.target.dragged {
+	background-color: #888;
+}
+
+.center {
+	text-align: center;
+	width: fit-content;
+	margin: auto;
+}
+
+.center span {
+	margin-bottom: 1rem;
+	display: block;
+}
+
+.center>*:first-child {
+	margin-top: 1rem;
+}
+
+.error {
+	color: red;
+}
+</style>
